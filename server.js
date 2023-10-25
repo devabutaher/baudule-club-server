cae69afe7cb247e8ff7af76bbd668bc40f93bc29
Abu Taher <writerabutaher@gmail.com>
2023-10-25 23:12:20 +0600

    connectDB

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-require("dotenv").config();
+const connectDB = require("./config/connectDB");
 const { errorHandler, errorRouter } = require("./middleware/errorHandler");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors());
 app.use(morgan("dev"));
+app.use(cors());
+connectDB();
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: `Server listening on port ${port}` });
