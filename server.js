9ffe150fdb955bac165e3a7d1a23ca73571ac7a1
Abu Taher <writerabutaher@gmail.com>
2023-10-25 07:41:56 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: `Server listening on port ${port}` });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
