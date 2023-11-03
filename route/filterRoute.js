2821adf1cb29178774d083795e4551ef86f571a8
Abu Taher <writerabutaher@gmail.com>
2023-11-03 23:43:31 +0600

    done

--- route/filterRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const filterPackages = require("../controller/filterController");
-const router = express.Router();
-
-router.route("/").get(filterPackages);
-
-module.exports = router;
---
+++ view/api.js
@@ -0,0 +1,16 @@
+/*
+
+Categories
+
+Get all categories & Create = http://localhost:5000/categories
+Update & Delete = http://localhost:5000/categories/:id
+
+
+
+Packages
+
+Get all packages & Create = http://localhost:5000/packages
+Get package by ID & Update & Delete = http://localhost:5000/packages/:id
+Filter packages by Categories & Division & Duration = http://localhost:5000/packages?categories=forest,beach&division=sylhet&duration=1-5
+
+*/
